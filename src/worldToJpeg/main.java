05f17b55f1b11741d7964150d9148a3317abeb62
angelless <dongkyu91622@gmail.com>
2016-10-02 16:47:41 +0900

    Update main.java

--- src/worldToJpeg/main.java
+++ src/worldToJpeg/main.java
@@ -18,6 +18,7 @@ public class main extends PluginBase implements Listener {
 
 	@Override
 	public void onEnable() {
+
 		this.getServer().getPluginManager().registerEvents(this, this);
 		this.getDataFolder().mkdirs();
 		this.getServer().getScheduler().scheduleAsyncTask(new AsyncTask() {
@@ -29,9 +30,6 @@ public class main extends PluginBase implements Listener {
 					image = new BufferedImage(600, 600, BufferedImage.TYPE_INT_RGB);
 					Graphics2D graphics = image.createGraphics();
 					Position spawn = getServer().getDefaultLevel().getSafeSpawn();
-					/**
-					*스폰위치 반경으로 x,z좌표를 ±300 만큼 graphics인스턴스에 넣어줍니다
-					*/
 					for (int x = 0; x < 600; x++) {
 						for (int y = 0; y < 600; y++) {
 							graphics.setColor(getServer().getDefaultLevel().getMapColorAt(spawn.getFloorX() - 300 + x,
@@ -43,6 +41,7 @@ public class main extends PluginBase implements Listener {
 					 * 플레이어를 인식하면 해당 플레이어의 위치와 그 닉네임을 추가로 이미지에 반영합니다
 					 * 해당 기능을 원치않을경우 작성하지 않으시면 됩니다
 					 */
+					/*
 					for (Player player : getServer().getOnlinePlayers().values()) {
 						int x = player.getPosition().getFloorX();
 						int y = player.getPosition().getFloorZ();
@@ -52,13 +51,16 @@ public class main extends PluginBase implements Listener {
 								|| y <= spawn1.getFloorZ() + 300 && y >= spawn1.getFloorZ() - 300) {
 
 							graphics.setColor(Color.RED);
-							graphics.fillRect((spawn.getFloorX() - x) + 299, (spawn.getFloorZ() - y) + 299, 2, 2);
+							graphics.fillRect((spawn.getFloorX() - x) + 299, (spawn.getFloorZ() - y) + 299,
+									// (spawn.getFloorX() - x) + 300,
+									// (spawn.getFloorZ() - y) + 300
+									2, 2);
 							graphics.setColor(Color.BLACK);
 							graphics.drawString(player.getName(), (spawn.getFloorX() - x) + 300,
 									(spawn.getFloorZ() - y) + 296);
 
 						}
-					}
+					}*/
 					try {
 						File file = new File(getDataFolder() + "/world.jpeg");
 						ImageIO.write(image, "jpeg", file);
